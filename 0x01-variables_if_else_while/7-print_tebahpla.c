ffa9738fcbbf14da736fd2e92dc2d51d1d803a64
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-09 13:33:47 -0700

    A

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet at reverse
+ *
+ * Return: Always (Success)
+ */
+
+int n;
+
+{
+
+for ( n= 'z' ; n => 'a' ; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
