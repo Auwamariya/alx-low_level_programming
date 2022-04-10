cbe4308e0c37221c6a6527e4d2d81549d7e266ed
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-10 03:18:23 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,8 @@
 /**
  * main - entry point
  *
- * Description: print numbers digit numbers of base 10 starting from 0;
+ * Description: print numbers digit numbers of base 10
+ * starting from 0;
  * Return: 0
  */
 
@@ -12,7 +13,7 @@ int main(void);
 {
 int num;
 for (num = 0; num < 10; num++)
-putchar((num % 10) + '0');
+putchar(num + '0');
 putchar('\n');
 return (0);
 }
