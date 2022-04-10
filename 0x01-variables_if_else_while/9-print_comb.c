786dba2041126872f4a793a8424be63879fccd6e
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-10 05:18:33 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@
 int main(void)
 
 {
-int num;
+int number;
 
-for (num = '0'; num <= '9'; num++)
+for (number = '0'; number <= '9'; number++)
 {
-putchar(num + '0');
+putchar(number + '0');
 
-if (num < '9')
+if (number < '9')
 {
 putchar(',');
 putchar(' ');
