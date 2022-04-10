23de0e64c9bdd375a9db0756e7a778316d5246fc
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-10 10:59:55 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits.
+ * Print only the smallest combination of three digits
+ * Numbers should be printed in ascending order, with three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int hundreds;
+int tens;
+int ones;
+
+for (hundreds = 0; hundreds <= 9; hundreds++)
+{
+for (tens = 1 ; tens <= 9; tens++)
+{
+for (ones = 2; ones <= 9; ones++)
+{
+putchar(hudreds + '0');
+putchar(tens + '0');
+putchar(ones + '0');
+
+if (hundreds < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
