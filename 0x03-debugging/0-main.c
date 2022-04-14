04188a5a70e931d9cdcd498b9e7e8af90a13bcb4
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-14 00:45:51 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,18 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - Test function for positive or negative
+ * 
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-{
-int n;
-n = 98;
-positive_or_negative(n)
 
-if (n > 0)
 {
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
+
+int i;
+i = 0;
+positive_or_negative(i);
+
 return (0);
 }
