16b4bcd5e678fca0891cb45ca2da89db6379334e
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-20 10:49:35 -0700

    Prints

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,14 @@
  */
 void print_rev(char *s)
 {
-	int count = 0;
-
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
+int count = 0;
+while (count >= 0)
+{
+if (s[count] == '\0')
+break;
+count++;
+}
 for (count--; count >= 0; count--)
-		_putchar(s[count]);
-	_putchar('\n');
+_putchar(s[count]);
+_putchar('\n');
 }
