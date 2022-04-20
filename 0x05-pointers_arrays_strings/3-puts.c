561c86b470b6d901ff80086efc96d26e8a0cb925
Auwamariya <101397208+Auwamariya@users.noreply.github.com>
2022-04-20 18:06:14 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,15 +3,10 @@
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
-
 void _puts(char *str)
 {
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
- i++;
- {
-  _putchar('\n'); 
- 
+	while (*str != '\0')
+		_putchar(*str++);
+
+	_putchar('\n');
 }
