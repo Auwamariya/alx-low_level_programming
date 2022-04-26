79b5699ef80f4e36d648edcc249d04d87789fe57
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-26 08:55:23 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,17 @@
+#include "main.h"
+#include <stdio.h>
 
+/* a function that fills memory with a constant byte.
+ *
+ * Return s
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+	return (s);
+}
