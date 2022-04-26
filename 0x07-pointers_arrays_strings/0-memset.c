6f0bb575ed150e120672b1b12d1ddc60c5ca8d50
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-26 08:50:13 -0700

    my commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,1 @@
-#include "main.h"
 
-/*
- * a function that fills memory with a constant byte.
- * return s
- */
-
-char *_memset(char *s, char b, unsigned int n);
-
-{
-
-char *ptr = s;
-while (n--)
-	*s++ = b;
-return (s);
-}
