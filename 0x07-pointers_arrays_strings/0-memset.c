cac4ab0479b4338eac2d24b77c3dfa5ee9667b55
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-04-27 10:14:00 -0700

    commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
+/*
  * _memset - fills memory with a constant byte,
  * @s: memory area.
  * @b: constant byte.
@@ -10,11 +10,10 @@
  */
 
 char *_memset(char *s, char b, unsigned int n)
+
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-
-	return (s);
+unsigned int i;
+for (i = 0; i < n; i++)
+*(s + i) =  b;
+return (s);
 }
