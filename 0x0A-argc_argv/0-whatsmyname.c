34aa529c90f40202db39358e7442e9493a2fc2f5
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-05-04 11:50:14 -0700

    My work

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - display name of program
- * @argc:int
- * @argv:string
+ * @argc: Number of command line arguments
+ * @argv: Array name
  * Return:int
  */
+
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
-	{
-		printf("%s\n", argv[0]);
-	}
-	return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
