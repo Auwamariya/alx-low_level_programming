fdae27b41479bf4c8f543cbcf2bd6e61db930241
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-05-04 07:56:10 -0700

    My commit

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,7 +3,6 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -11,5 +10,9 @@
 int _putchar(char c)
 
 {
+
 return (write(1, &c, 1));
+
 }
+
+
