165bf3a83582d465e826e0c11444b3a22412c8b7
Auwamariya <101397208+Auwamariya@users.noreply.github.com>
2022-05-04 21:05:14 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,13 @@
  * @argv: Array name
  * Return:int
  */
-
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
-return (0);
+	int i;
+
+	for (i = 0; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
