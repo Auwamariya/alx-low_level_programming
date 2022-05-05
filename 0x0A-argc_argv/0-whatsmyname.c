2d5fc75ca194698f11cbf3ff5ca7b73ceb13470a
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-05-05 06:18:22 -0700

    Task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,7 @@
 
 /**
  * main - prints its name, followed by a new line.
+ * argc  attribute unused
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
