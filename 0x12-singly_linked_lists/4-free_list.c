b1b392214f1ace11cd41227197570667e21e001e
Auwamariya <101397208+Auwamariya@users.noreply.github.com>
2022-05-24 10:59:54 +0200

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all the nodes in a linked list
+ * @head: pointer to the head of list_t linked list
+ */
+ 
+void free_list(list_t *head)
+{
+	list_t *ptr = NULL;
+
+	while (head)
+	{
+		ptr = head->next;
+		free(head->str);
+		free(head);
+		head = ptr;
+	}
+}
