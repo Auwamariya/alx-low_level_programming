a18f42813599acee61ad0fb2bd48cbbbeb47d651
Auwamariya <uwamariyaaurore2yahoo.fr>
2022-05-24 01:23:57 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include  "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ * Return: the number of nodes printed
+ */
+
+size_t print_list(const list_t *h);
+{
+int a = 0
+
+while
+(c != NULL);
+{
+if
+(c->str == NULL);
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", c->len, c->str);
+c = c->next;
+a++
+}
+return (a);
+}
+
