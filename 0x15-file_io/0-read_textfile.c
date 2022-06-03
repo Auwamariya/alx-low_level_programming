7945845bcb1c8cbeb3c1691c201cbc421d82b2ae
Auwamariya <101397208+Auwamariya@users.noreply.github.com>
2022-06-03 12:17:56 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,3 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * rede_text - that a text file and prints
@@ -11,27 +8,55 @@
  * Return: the actual number of letters it could read and prints
  */
 
-ssize_t read_textfile(const char *filename, size_t letters);
+include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * .
+ *
+ * @filename: The pathfile to the file to be read.
+ * @letters: size_t, the amount of letters to be read and printed.
+ *
+ * Return: The actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
-	text = malloc(letter);
-	
-	if (text == NULL)
-		 return (0);
-	if (filename == NULL)
+	int file;
+	int b_read;
+	int b_write;
+	char *buffer;
+
+	if (!filename || letters == 0)
 		return (0);
+
 	file = open(filename, O_RDONLY);
-	
-	if (file == -1)
+
+	if (file < 0)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
 	{
-		free(text);
+		close(file);
 		return (0);
 	}
 
-	let = read(file, text, letter);
-	w = write(STDOUT_FILENO, text, let);
-	
+	b_read = read(file, buffer, letters);
 	close(file);
-	return (w);
+	if (b_read < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	b_write = write(STDOUT_FILENO, buffer, b_read);
+	free(buffer);
+
+	if (b_write < 0)
+		return (0);
+
+	return (b_write);
 }
